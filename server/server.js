d8f05f55f7cbe5f1f9326a6e8612f516c1371acf
Sumit <akoliyasumit@hotmail.com>
2022-02-08 00:11:27 +0530

    fetch from mysql to server and console log

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json())
 
+app.get('/api/get', (req,res)=>{
+    const sqlInsert = 
+    "SELECT * FROM movie_reviews;"
+    //it is compulsory to write err first and result second
+    db.query(sqlInsert,(err,result)=>{
+        console.log(`sqlInsert: `, sqlInsert )
+        console.log(`result: `,result)
+        console.log(`err: `,err)
+        res.send(result)
+    })
+})
+
 app.post('/api/insert',(req,res)=>{
     const movieName = req.body.movieName
     const movieReview = req.body.movieReview
@@ -27,16 +39,9 @@ app.post('/api/insert',(req,res)=>{
         console.log(`sqlInsert: `, sqlInsert )
         console.log(`result: `,result)
         console.log(`err: `,err)
+        
      })
 })
 
 app.listen(3001,()=> console.log(`listening on port 3001`))
 
-// app.get('/',(req,res)=> {
-//     const sqlInsert = 
-//     "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie');"
-//     // db.query(sqlInsert, (err,result)=>{
-//     //     res.send('hi')
-//     // })
-//     res.send('hi')
-// })
